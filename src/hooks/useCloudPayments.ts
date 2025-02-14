ae2ad8be85ecac37f57b4c4ff2d9a6d4dbd80f78
aipravo <aipravo.kz@gmail.com>
2025-02-14 11:56:39 +0500

    Update useCloudPayments.ts

--- src/hooks/useCloudPayments.ts
+++ src/hooks/useCloudPayments.ts
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 const useCloudPayments = () => {
     useEffect(() => {
         const script = document.createElement("script");
-        script.src = "https://widget.cloudpayments.ru/bundles/cloudpayments.js";
+        script.src = "https://widget.tiptoppay.kz/bundles/widget.js";
         script.async = true;
         document.body.appendChild(script);
 
